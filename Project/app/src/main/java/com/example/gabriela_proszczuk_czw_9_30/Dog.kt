db37c6022f876b5f7e7bf688711f758b17a71ee1
unknown <gabrielaproszczuk@gmail.com>
2021-04-17 00:08:33 +0200

    added dogProfile layout

--- Project/app/src/main/java/com/example/gabriela_proszczuk_czw_9_30/Dog.kt
+++ Project/app/src/main/java/com/example/gabriela_proszczuk_czw_9_30/Dog.kt
@@ -1,5 +1,5 @@
 package com.example.gabriela_proszczuk_czw_9_30
 
-class Dog {
+class Dog(var name: String, var city: String) {
 
 }
--- Project/app/src/main/java/com/example/project/MainActivity.kt
+++ Project/app/src/main/java/com/example/project/MainActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.gabriela_proszczuk_czw_9_30.DataHolder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //DataHolder.prepare(applicationContext)
+        DataHolder.prepare(applicationContext)
     }
 
     fun toMap(view: View) {
