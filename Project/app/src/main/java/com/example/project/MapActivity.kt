63b2d5505c1b74299b2d47f1f3a5485db40fd072
unknown <gabrielaproszczuk@gmail.com>
2021-04-19 22:39:26 +0200

    added google map key

--- Project/app/src/main/java/com/example/project/MapActivity.kt
+++ Project/app/src/main/java/com/example/project/MapActivity.kt
@@ -2,10 +2,21 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 class MapActivity : AppCompatActivity() {
+
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+        })
     }
 }
